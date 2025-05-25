9a2a7c3a63a15ae5a5e59023922e40a02ef04c2e
Mohammed Khair <55262573+Mohammed12Khair@users.noreply.github.com>
2025-05-25 22:30:47 +0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's game command-line utility for administrative tasks."""
 import os
 import sys
 
@@ -18,4 +18,5 @@ def main():
 
 
 if __name__ == '__main__':
+
     main()
